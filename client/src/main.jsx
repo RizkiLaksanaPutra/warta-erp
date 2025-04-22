b04aadc298cbaf2d78b0984d16fe0b14578b959f
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-04-22 21:25:23 +0700

    Replace App component with Home component and add AuthContext file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Login from "./pages/Login.jsx";
-import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
