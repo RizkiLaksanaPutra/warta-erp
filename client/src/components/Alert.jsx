6842629ac8a14b04c5ef5a15df1d5a6dc48b7d89
Rizki Laksana Putra <rizkilaksana007@gmail.com>
2025-04-24 17:43:46 +0700

    check point

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Alert = ({props}) => {
   return (
-    <div className="rounded-md bg-red-50 p-4 my-4">
+    <div className="rounded-md bg-slate-50 p-4 my-4">
       <div className="flex">
         <div className="flex-0">
-          <svg className="h-5 w-5 text-red-400">
+          <svg className="h-5 w-5 text-black">
             <path
               fillRule="evenodd"
               d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
@@ -13,7 +13,7 @@ const Alert = ({props}) => {
             ></path>
           </svg>
         </div>
-        <p className="text-sm font-medium text-red-800 ml-3">
+        <p className="text-sm font-medium text-black ml-3">
           {props}
         </p>
       </div>
