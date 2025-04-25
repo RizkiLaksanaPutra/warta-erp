b756d1dd76200cc78b4e8a583feddf5d13faed45
Rizki Laksana Putra <120088587+RizkiLaksanaPutra@users.noreply.github.com>
2025-04-25 16:23:37 +0700

    check point

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -36,17 +36,9 @@ const Layout = () => {
         </div>
         <button
           onClick={handleSignOut}
-          className="cursor-pointer border border-black p-2 m-2 flex items-center justify-between"
+          className="cursor-pointer border border-black p-2 m-2"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="black"
-            width={30}
-          >
-            <path d="M4 18H6V20H18V4H6V6H4V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V18ZM6 11H13V13H6V16L1 12L6 8V11Z"></path>
-          </svg>
-          <span className="flex-1 text-center">Log out</span>
+          Sign Out
         </button>
       </aside>
       <div className="w-[85%] flex flex-col">
